ceca5977b2f4b1fde58c489894790e98ea655de7
Danilo Klimenko <daniilklimenko@meta.ua>
2017-03-27 12:23:27 +0300

    Timetable Md size support added

--- src/AntFlight/wwwroot/js/timeTable.js
+++ src/AntFlight/wwwroot/js/timeTable.js
@@ -1,62 +1,30 @@
-﻿var GetMargin = polymorph(
-    function (months, monthParts) {
-        /*-11 = -15(basic) + 4 (left space + left border)*/
-        /*51 - width of cell*/
-        /*13 - width of cell part(12) + 1(border-width)*/
-        return -11 + 51 * months + 13 * monthParts;
-    }
-);
-
+﻿//var GetMargin = polymorph(
+//    function (months, monthParts) {
+//        /*-11 = -15(basic) + 4 (left space + left border)*/
+//        /*51 - width of cell*/
+//        /*13 - width of cell part(12) + 1(border-width)*/
+//        return -11 + 51 * months + 13 * monthParts;
+//    }
+//);
 $(document).ready(function () {
-    //var flight_line = $('.flights_timetable tbody tr').first().find('td:last');
-
-    //flight_line.css("background-color", "yellow");
-
-    //flight_line.append($('<img>', { src: '/images/flight_mark.png', style: "margin-left:" + GetMargin(1, 4) + "px;" }));
-    //console.log('margin-left'+ flight_line.find('img').first().css('margin-left'));
-
-    //flight_line.append($('<img>', { src: '/images/flight_mark.png', style: "margin-left:" + 0 + "px;" }));
-    //var $tableBody = $('.flights_timetable tbody');
-
-
-
-    //$.ajax({
-    //    type: "POST",
-    //    url: "/Home/TestJson",
-    //    dataType: "JSON",
-    //    success: function (data) {
-    //        $.each(data, function (key, val) {
-    //            console.log(key + "-" + JSON.stringify(val));
-    //            var $currTimeline = $tableBody.find("tr td:contains('" + val.name + "')").next();
-
-    //            val.months.forEach(function (timeline, monthNum) {
-    //                console.log(monthNum + " month " + timeline);
-
-    //                timeline.forEach(function (monthPartVal, monthPartNum) {
-    //                        $currTimeline.append($('<img>',
-    //                                        {
-    //                                            src: '/images/flight_mark.png',
-    //                                            style: "margin-left:" + GetMargin(monthNum , monthPartNum) + "px;"
-    //                                        }));
-    //                    }
-    //                });
-    //            });
-
-    //        });
-    //    }
-    //})
-
-    //function getMargin(months, spaces) {
-    //    /*47 - ширина месяца , 565/12*/
-    //    /*46/4 - ширина ячейки , (47-1)/4*/
-    //    return -14 + months*47 + (46/4)*spaces;
-    //}
-
-    /// <summary>
-    /// s this instance.
-    /// </summary>
-    /// <returns></returns>
     var $tableBody = $('.flights_timetable tbody');
+    var widthOfCell, widthOfCellPart, widthOfLastCellPart, basicLeftSpace;
+
+    if (window.matchMedia('(max-width:1199px)').matches) {
+        widthOfCell = 42;
+        widthOfCellPart = 10;
+        widthOfLastCellPart = 8;
+        basicLeftSpace = -12;
+    } else if (window.matchMedia('(min-width:1200px)').matches) {
+        console.log('resolution >= 1200');
+        widthOfCell = 51;
+        widthOfCellPart = 12;
+        widthOfLastCellPart = 11;
+        basicLeftSpace = -11;
+    } else {
+        console.log('No resolution found');
+    }
+
 
     $.ajax({
         type: "POST",
@@ -70,26 +38,29 @@ $(document).ready(function () {
                 var $currTimeline = $tableBody.find("tr td:contains('" + val.name + "')").next();
 
                 /*all months*/
-                val.months.forEach(function (timeline, monthNum) {
-                    console.log(monthNum + " month " + timeline);
+                val.months.forEach(function (currMonth, monthNum) {
+                    console.log(monthNum + " month " + currMonth);
                     /*curr month parts*/
-                    timeline.forEach(function (monthPartVal, monthPartNum) {
+                    currMonth.forEach(function (monthPartVal, monthPartNum) {
                         /*Append img*/
                         if (monthPartVal != 0) {
                             var $message = $('<img>', {
                                 src: '/images/flight_mark.png',
                                 style: "margin-left:" + GetMargin(monthNum, monthPartNum) + "px;"
                             });
-
+                            /*if last part of month*/
                             if (monthPartNum == 3) {
-                                if (monthNum != 10 && val.months[monthNum+1][0]) {
-                                    $message.css("width", "12px");
+                                /*not last month && */
+                                if (monthNum != 10 && val.months[monthNum + 1][0] != 0) {
+                                    $message.css("width", (widthOfLastCellPart + 1) + "px");
                                 } else {
-                                    $message.css("width", "11px");
+                                    $message.css("width", widthOfLastCellPart + "px");
                                 }
                             } else {
-                                if (timeline[monthPartNum + 1] != 0) {
-                                    $message.css("width", "13px");
+                                if (currMonth[monthPartNum + 1] != 0) {
+                                    $message.css("width", (widthOfCellPart + 1) + "px");
+                                } else {
+                                    $message.css("width", widthOfCellPart + "px");
                                 }
                             }
 
@@ -101,5 +72,9 @@ $(document).ready(function () {
 
         }
     });
+
+    function GetMargin(months, monthParts) {
+        return basicLeftSpace + widthOfCell * months + (widthOfCellPart + 1) * monthParts;
+    }
 });
 
