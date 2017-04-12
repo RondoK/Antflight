6298b759eacae6983e33f1355abc093aea0a96fe
Danilo Klimenko <daniilklimenko@meta.ua>
2017-04-12 20:20:35 +0300

    Added fields for Frlight Message Description

--- src/AntFlight/Migrations/20170412163655_FlightMessage DescriptionUpdate.Designer.cs
+++ src/AntFlight/Migrations/20170412163655_FlightMessage DescriptionUpdate.Designer.cs
@@ -8,9 +8,10 @@ using AntFlight.Data;
 namespace AntFlight.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170412163655_FlightMessage DescriptionUpdate")]
+    partial class FlightMessageDescriptionUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
--- src/AntFlight/Models/FlightMessages/FlightMessageDiscription.cs
+++ src/AntFlight/Models/FlightMessages/FlightMessageDiscription.cs
@@ -14,7 +14,32 @@ namespace AntFlight.Models.FlightMessages
         [ForeignKey("FlightMessage")]
         public int FlightMessageId { get; set; } 
         public FlightMessage FlightMessage { get; set; }
+        [Required]
+        [Display(Name ="Сила лёта")]
+        [MaxLength(50)]
+        public string FlightIntensity { get; set; }
 
+        [Display(Name = "Температура")]
+        [MaxLength(30)]
+        public string Temperature { get; set; }
+
+        [Display(Name = "Осадки")]
+        [MaxLength(30)]
+        public string Precipitation { get; set; }
+
+        [Display(Name = "Небо")]
+        [MaxLength(30)]
+        public string Sky { get; set; }
+
+        [Display(Name = "Ветер")]
+        [MaxLength(30)]
+        public string Wind { get; set; }
+
+        [Display(Name = "Местность")]
+        [MaxLength(30)]
+        public string Terrain { get; set; }
+
+        [Display(Name ="Описание")]
         public string Description { get; set; }
     }
 }
